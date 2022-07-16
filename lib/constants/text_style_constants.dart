c0ac842de8bceb03e83e3dbbdd9c911b72f990fc
Antonio Cabrera <acabrerag94@gmail.com>
2022-07-16 20:43:11 +0200

    Added new text styles constants

--- lib/constants/text_style_constants.dart
+++ lib/constants/text_style_constants.dart
@@ -9,7 +9,22 @@ class TextStyleConstants {
     color: ColorConstants.DARKER_GREY,
     fontSize: 23,
   );
+  static const TITLE_1_BLUE = TextStyle(
+    fontWeight: FontWeight.w700,
+    color: ColorConstants.BLUE,
+    fontSize: 23,
+  );
+  static const SUBTITLE_1_GREY = TextStyle(
+    color: ColorConstants.GREY,
+  );
+  static const SUBTITLE_1_DARK_GREY = TextStyle(
+    color: ColorConstants.DARK_GREY,
+  );
   static const SUBTITLE_1_DARKER_GREY = TextStyle(
     color: ColorConstants.DARKER_GREY,
+  );
+  static const SUBTITLE_1_DARKER_GREY_S = TextStyle(
+    color: ColorConstants.DARKER_GREY,
+    fontSize: 16,
   );
 }
